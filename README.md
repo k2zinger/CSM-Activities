762dd0513ca622ca76c2ea711aaf0788801c7000
k2zinger <k2zinger@gmail.com>
2019-04-22 15:50:03 -0400

    GetColumnLetter initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-CSM-Activities
+# CSM-Activities
+A collection of Activities for UiPath Studio, created by CSMs
+[![Build status](https://ci.appveyor.com/api/projects/status/pvwmaie8e6si2xmx/branch/master?svg=true)](https://ci.appveyor.com/project/k2zinger/csm-activities/branch/master)
+
+## Installation
+Studio -> Manage Packages -> Community -> (Search) CSM-Activities -> Install -> Save
+## Activities
+### Microsoft Excel
+`GetColumnLetter`: Get the Column Letter within an Excel Worksheet by providing a DataTable and Column Name (case sensitive)
---
+++ UiPathTeam.Activities.Test/GetColumnLetterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UiPathTeam.Activities.Test
+{
+    [TestClass]
+    public class GetColumnLetterTest
+    {
+        [TestMethod]
+        public void CalculateColumnLetterTest()
+        {
+            var columns = new [] { new object[] { 0, "A" }, new object[] { 26, "AA" }, new object[] { 702, "AAA" }, new object[] { 4082, "FAA" }, new object[] { 16383, "XFD" } };
+
+            foreach(var col in columns)
+            {
+                Assert.AreEqual(GetColumnLetter.CalculateColumnLetter((int)col[0] + 1), col[1]);
+            }
+        }
+    }
+}
