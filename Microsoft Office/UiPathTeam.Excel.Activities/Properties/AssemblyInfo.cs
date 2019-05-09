915cca4c5eae57f702f09ab49adf2ed48e5e3559
k2zinger <k2zinger@gmail.com>
2019-05-09 14:07:03 -0400

    update assembly info

--- Microsoft Office/UiPathTeam.Excel.Activities/Properties/AssemblyInfo.cs
+++ Microsoft Office/UiPathTeam.Excel.Activities/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Windows.Markup;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Excel.Activities")]
-[assembly: AssemblyDescription("Excel activities")]
+[assembly: AssemblyTitle("UiPathTeam.Excel.Activities")]
+[assembly: AssemblyDescription("UiPathTeam Excel activities")]
 [assembly: AssemblyConfiguration("")]
-[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities", "Excel.Activities")]
+[assembly: XmlnsDefinition("http://schemas.uipath.com/workflow/activities", "UiPathTeam.Excel.Activities")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
