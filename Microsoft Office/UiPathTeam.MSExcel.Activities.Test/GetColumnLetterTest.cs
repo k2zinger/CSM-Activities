4c7bf50c5390b48338c0d159865287532a59a74c
k2zinger <k2zinger@gmail.com>
2021-07-02 13:00:06 -0400

    add column letter lookup by column index (starting with index 1)

--- Microsoft Office/UiPathTeam.MSExcel.Activities.Test/GetColumnLetterTest.cs
+++ Microsoft Office/UiPathTeam.MSExcel.Activities.Test/GetColumnLetterTest.cs
@@ -15,5 +15,16 @@ namespace UiPathTeam.MSExcel.Activities.Test
                 Assert.AreEqual(GetColumnLetter.CalculateColumnLetter((int)col[0] + 1), col[1]);
             }
         }
+
+        [TestMethod]
+        public void RetrieveColumnLetterByIndexTest()
+        {
+            var columns = new[] { new object[] { 1, "A" }, new object[] { 27, "AA" }, new object[] { 703, "AAA" }, new object[] { 4083, "FAA" }, new object[] { 16384, "XFD" } };
+
+            foreach (var col in columns)
+            {
+                Assert.AreEqual(GetColumnLetter. CalculateColumnLetter((int)col[0]), col[1]);
+            }
+        }
     }
 }
--- Microsoft Office/UiPathTeam.MSExcel.Activities/GetColumnLetter.cs
+++ Microsoft Office/UiPathTeam.MSExcel.Activities/GetColumnLetter.cs
@@ -5,14 +5,20 @@ using System.Data;
 
 namespace UiPathTeam.MSExcel.Activities
 {
-    [DisplayName("Get Column Letter"), Description("Get the Column Letter within an Excel Worksheet by providing a DataTable and Column Name (case sensitive)")]
+    [DisplayName("Get Column Letter"), Description("Get the Column Letter within an Excel Worksheet by providing a DataTable and Column Name (case sensitive) or Index of Column (starts with index 1)")]
     public class GetColumnLetter : CodeActivity
     {
         #region Properties
 
         [Category("Input"), Description("Name of Column (exact match)")]
         [RequiredArgument]
+        [OverloadGroup("G1")]
         public InArgument<string> ColumnName { get; set; }
+
+        [Category("Input"), Description("Index of Column (starting with index 1)")]
+        [RequiredArgument]
+        [OverloadGroup("G2")]
+        public InArgument<int> ColumnNumber { get; set; }
 
         [Category("Input"), Description("DataTable to obtain Column Letter from")]
         [RequiredArgument]
@@ -29,14 +35,23 @@ namespace UiPathTeam.MSExcel.Activities
         {
             string cn = ColumnName.Get(context);
             DataTable dt = DataTable.Get(context);
+            int ci;
 
-            if (!dt.Columns.Contains(cn))
+            if (null == cn)
             {
-                throw new ArgumentException("Column '" + cn + "' was not found");
+                ci = ColumnNumber.Get(context);
+            }
+            else
+            {
+                if (!dt.Columns.Contains(cn))
+                {
+                    throw new ArgumentException("Column '" + cn + "' was not found");
+                }
+
+                //add 1 to the column index since we always start with column index 0
+                ci = dt.Columns.IndexOf(cn) + 1;
             }
 
-            //add 1 to the column index since we always start with column index 0
-            int ci = dt.Columns.IndexOf(cn) + 1;
             ColumnLetter.Set(context, CalculateColumnLetter(ci));
         }
 
